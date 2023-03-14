9768fa0c7351e26d402e4350987c906ad5c710bd
Jonas Regh <Jonas Regh>
2023-03-14 15:39:48 +0100

    Interactable Tooltip hinzugefügt

--- Unity/Escape_Your_Dreams/Assets/Scripts/Tooltips.cs
+++ Unity/Escape_Your_Dreams/Assets/Scripts/Tooltips.cs
@@ -9,18 +9,30 @@ public class Tooltips : MonoBehaviour
     [SerializeField]
     private Transform cameraTransform;
     LayerMask grabbable;
+    LayerMask interactable;
     private Transform grabTooltip;
+    private Transform interactTooltip;
 
     private void Start()
     {
         grabbable = LayerMask.GetMask("Grabbable");
+        interactable = LayerMask.GetMask("Interactable");
         grabTooltip = TooltipContainer.GetChild(0);
+        interactTooltip = TooltipContainer.GetChild(1);
     }
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Linecast(cameraTransform.position, cameraTransform.position + cameraTransform.forward * 2.5f, out hit) && grabbable == (grabbable | (1 << hit.transform.gameObject.layer)))
+        if (Physics.Linecast(cameraTransform.position, cameraTransform.position + cameraTransform.forward * GrabObject.maxHoldAreaDistance, out hit) && grabbable == (grabbable | (1 << hit.transform.gameObject.layer)))
+        {
+            grabTooltip.localScale = Vector3.one;
+        }
+        else
+        {
+            grabTooltip.localScale = Vector3.zero;
+        }
+        if (Physics.Linecast(cameraTransform.position, cameraTransform.position + cameraTransform.forward * 2f, out hit) && grabbable == (grabbable | (1 << hit.transform.gameObject.layer)))
         {
             grabTooltip.localScale = Vector3.one;
         }
