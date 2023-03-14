abaf3058b324960bc241dbf6e9300dd363b47099
Jonas Regh <Jonas Regh>
2023-03-14 16:07:30 +0100

    Interact einseitig fertig & LayerChange erstellt

--- Unity/Escape_Your_Dreams/Assets/Scripts/Interact.cs
+++ Unity/Escape_Your_Dreams/Assets/Scripts/Interact.cs
@@ -7,21 +7,15 @@ public class Interact : MonoBehaviour
     [SerializeField]
     private Transform cameraTransform;
     public readonly float maxInteractionDistance = 2;
+    [SerializeField]
     private LayerMask targetLayer;
     private void OnInteract()
     {
         RaycastHit hit;
         if (Physics.Linecast(cameraTransform.position, cameraTransform.position + cameraTransform.forward * maxInteractionDistance, out hit) && targetLayer == (targetLayer | (1 << hit.transform.gameObject.layer)))
         {
-            target = hit.transform.gameObject;
-            originalTargetPosition = hit.transform.position;
-            targetParent = target.transform.parent;
-            target.transform.parent = holdArea;
-            targetRB = target.GetComponent<Rigidbody>();
-            targetRB.useGravity = false;
-            targetRB.drag = 10;
-            targetRB.freezeRotation = true;
-            target.transform.gameObject.layer = (int)Mathf.Log(grabbed.value, 2);
+            GameObject target = hit.transform.gameObject;
+            target.SendMessage("Interacted");
         }
     }
 }
