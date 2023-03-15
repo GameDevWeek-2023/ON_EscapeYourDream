845c1f4fdf9e03ed530943d40cc47e3dc8857436
Jonas Regh <Jonas Regh>
2023-03-15 16:16:46 +0100

    PortalScript umbenannt

--- Unity/Escape_Your_Dreams/Assets/Scripts/Portal.cs
+++ Unity/Escape_Your_Dreams/Assets/Scripts/Portal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PostItPortal : MonoBehaviour
+public class Portal : MonoBehaviour
 {
     [SerializeField]
     private Transform otherPortal;
